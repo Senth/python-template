dd0719e69c8b3931f830bfe8de4f39237d1134c1
Senth <senth.wallace@gmail.com>
2021-06-18 18:51:12 +0200

    Make config optional and use scm_version

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-module_name = "{{cookiecutter.module_name}}"
 project_slug = "{{cookiecutter.project_slug}}"
+module_name = project_slug.replace('-', "_")
 
 setup(
     name=project_slug,
-    version="{{cookiecutter.version}}",
+    use_scm_version=True,
     url=f"https://github.com/{{cookiecutter.github_username}}/{project_slug}",
     license="MIT",
     author="{{cookiecutter.full_name}}",
@@ -16,15 +16,18 @@ setup(
     description="{{cookiecutter.project_description}}",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=[module_name],
+    packages=find_packages(),
     entry_points={"console_scripts": [f"{project_slug}={module_name}.__main__:main"]},
+{%- if cookiecutter.external_config == "y" -%}
     include_package_data=True,
     data_files=[(f"config/{project_slug}", ["config/config.example.py"])],
+{%- endif -%}
     install_requires=[],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Licence :: OSI Approved :: MIT Licence",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     python_requires=">=3.8",
 )
