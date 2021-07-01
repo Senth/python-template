4c7c34cd8a903d5738ab79de6b3113ec410c2600
Senth <senth.wallace@gmail.com>
2021-07-01 21:52:42 +0200

    🐛 Fix License in classifiers (should be spelled with 's' not 'c')

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Licence :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
